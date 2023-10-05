808250a1aadc39e736fa6590bd8d02bf933ff532
Lucas <szanonn@gmail.com>
2023-10-05 13:14:56 -0300

    feat: configured vuetify

--- client-nuxt3/nuxt.config.ts
+++ client-nuxt3/nuxt.config.ts
@@ -1,4 +1,24 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: false },
+  ssr: false,
+  css: ["vuetify/lib/styles/main.sass"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    ssr: {
+      noExternal: ["vuetify"],
+    },
+  },
+  hooks: {
+    "vite:extendConfig": (config) => {
+      config.plugins?.push(
+        vuetify({
+          styles: { configFile: "./settings.scss" },
+        })
+      );
+    },
+  },
+});
