e128881c80f192af6d52024aa7a67d970390a40a
Lucas <szanonn@gmail.com>
2023-10-04 22:41:26 -0300

    feat: configuring axios

--- client-nuxt3/nuxt.config.ts
+++ client-nuxt3/nuxt.config.ts
@@ -1,6 +1,10 @@
 import vuetify from "vite-plugin-vuetify";
 
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/axios'],
+  plugins: [
+    '~/plugins/axios.ts',
+  ],
   devtools: { enabled: true },
   css: ["vuetify/lib/styles/main.sass"],
   build: {
@@ -11,9 +15,6 @@ export default defineNuxtConfig({
       noExternal: ["vuetify"],
     },
   },
-  plugins: [
-    '~/plugins/axios.js',
-  ],
   hooks: {
     "vite:extendConfig": (config) => {
       config.plugins?.push(
--- client-nuxt3/pages/index.vue
+++ client-nuxt3/pages/index.vue
@@ -1,32 +1,13 @@
-<!-- frontend/pages/tasks/index.vue -->
-
 <template>
-  <div>
-    <h1>Tarefas</h1>
-    <ul>
-      <li v-for="task in tasks" :key="task.id">{{ task.title }}</li>
-    </ul>
-  </div>
+  <h1>hello world</h1>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script>
+export default {
 
-export default defineComponent({
-  data() {
-    return {
-      tasks: [],
-    };
-  },
-  async asyncData() {
-    try {
-      // Faça uma solicitação GET para buscar as tarefas da API
-      const response = await this.$axios.get('/tasks');
-      return { tasks: response.data };
-    } catch (error) {
-      console.error('Erro ao buscar tarefas:', error);
-      return { tasks: [] };
-    }
-  },
-});
+}
 </script>
+
+<style>
+
+</style>
